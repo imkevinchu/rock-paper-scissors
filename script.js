1e60e4da982f6c84209f243e053cfd0aa4453b61
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-04-27 17:48:09 -0700

    track score

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const computerPlay = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-    const playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
     let result = "";
 
     if (playerSelection === computerSelection) {
@@ -21,36 +21,67 @@ const playRound = (playerSelection, computerSelection) => {
     else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
             result = "You win. Rock beats scissors.";
+            playerScore++;
         } else {
             result = "You lose. Paper beats rock.";
+            computerScore++;
         }
     }
     
     else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             result = "You win. Paper beats rock.";
+            playerScore++;
         } else {
             result = "You lose. Scissors beats paper.";
+            computerScore++;
         }
     }
 
     else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
             result = "You win. Scissors beats paper.";
+            playerScore++;
         } else {
             result = "You lose. Rock beats scissors.";
+            computerScore++;
         }
     }
 
     return result;
 }
 
+const calculateWinner = () => {
+    let result = "";
+
+    if (computerScore === playerScore) {
+        result = `Tied ${computerScore} to ${playerScore}`;
+    }
+
+    else if (computerScore > playerScore) {
+        result = `Computer won ${computerScore} to ${playerScore}`;
+    }
+
+    else {
+        result = `You won ${playerScore} to ${computerScore}`;
+    }
+
+    return result;
+}
+
 const game = () => {
+    computerScore = 0;
+    playerScore = 0;
+    
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("rock, paper, or scissors?");
         const computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
     }
+
+    console.log(calculateWinner())
 }
 
+var computerScore = 0;
+var playerScore = 0;
 game();
