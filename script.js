c45e39af1524e4ee37e1cf8f750201745eb09664
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-04-27 17:18:31 -0700

    implement rock paper scissors logic

--- script.js
+++ script.js
@@ -7,11 +7,46 @@ const computerPlay = () => {
     ];
 
     const randomMove = moves[Math.floor(Math.random() * moves.length)];
-    return randomMove
+    return randomMove;
 }
 
 const playRound = (playerSelection, computerSelection) => {
+    const playerMove = playerSelection.toLowerCase();
+    let result = "";
+
+    if (playerMove === computerSelection) {
+        result = `Tie. Both players selected ${playerMove}`;
+    } 
     
+    else if (playerMove === "rock") {
+        if (computerSelection === "scissors") {
+            result = "You win. Rock beats scissors.";
+        } else {
+            result = "You lose. Paper beats rock.";
+        }
+    }
+    
+    else if (playerMove === "paper") {
+        if (computerSelection === "rock") {
+            result = "You win. Paper beats rock.";
+        } else {
+            result = "You lose. Scissors beats paper.";
+        }
+    }
+
+    else if (playerMove === "scissors") {
+        if (computerSelection === "paper") {
+            result = "You win. Scissors beats paper.";
+        } else {
+            result = "You lose. Rock beats scissors.";
+        }
+    }
+
+    return result;
+}
+
+const game = () => {
+
 }
 
 const playerSelection = "rock";
